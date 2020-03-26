2aa107882d83ecb43c8a40605d787924a1fb94c3
Gabi80 <62231746+Gabi80@users.noreply.github.com>
2020-03-26 18:21:53 +0100

    Add files via upload

---
+++ first js code.js
@@ -0,0 +1,7 @@
+let name = "Gabriel";
+console.log("My name is: "+ name);
+const data = ["My courses are:", "Python", "HTML", "CSS", "JavaScript"];
+for (let i=1;i<=200;i++) {if (i%2===0){ data.push(i);
+}
+}
+console.log(data);
