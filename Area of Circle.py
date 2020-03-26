9251811c5ec8af319c6a37bdd5b1b30ff190b0aa
Gabi80 <62231746+Gabi80@users.noreply.github.com>
2020-03-26 04:51:27 +0100

    Add files via upload

---
+++ Area of Circle.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("Enter the radius of the circle "));
+area = math.pi  * math.pow(radius, 2);
+print("The area of the circle with radius "+str(radius)+" is: "+str(area))
